20b396fe7b9fdf9678d2a504570dc734925e8222
Donutz34 <100332013+Donutz34@users.noreply.github.com>
2026-09-17 18:11:06 -0400

    globals
    yuh

---
+++ caddy-godot/Globals.gd
@@ -0,0 +1,13 @@
+extends Node
+
+# Steam Variables
+var OWNED = false
+var ONLINE = false
+var STEAM_ID = 0
+var STEAM_NAME = ""
+
+# Lobby Variables
+var DATA
+var LOBBY_ID
+var LOBBY_MEMBERS = []
+var LOBBY_INVITE_ARG = false
